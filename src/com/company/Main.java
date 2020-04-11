9477277ab20a5f53fbcd41e88ebfdcd76fb27b92
Nikandrov Nikolai <60506141+Ixuzgold@users.noreply.github.com>
2020-04-11 14:49:06 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // show me you Stream API power
         System.out.println("test");
+        System.out.println("test2");
     }
 
 }
